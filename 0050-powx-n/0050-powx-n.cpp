4654ab34f49c89f4d99da92dd1955eefae022830
Aryan7300-hub <aryanjindal7300@gmail.com>
2026-07-29 22:45:11 +0530

    Time: 0 ms (100%), Space: 8.5 MB (93.46%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-       long exp = n;
-       if(exp<0){
-        x = 1/x;
-        exp = -exp;
-       }
-       return power(x, exp, 1);
+        long exp = n;
+        if(exp < 0){
+            x = 1/x;
+            exp = -exp;
+        }
+        return power(x, exp,1);
     }
 private:
     double power(double x, long n, double ans){
