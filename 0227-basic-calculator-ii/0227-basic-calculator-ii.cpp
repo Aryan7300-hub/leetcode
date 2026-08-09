18aae81f3a5baae6b70fd774e48a3081772f11d4
Aryan7300-hub <aryanjindal7300@gmail.com>
2026-08-09 23:30:57 +0530

    Time: 4 ms (81.32%), Space: 15.1 MB (60.74%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack<int>st;
+        int num = 0;
+        char op = '+';
+
+        for(int i = 0; i<=s.length(); i++){
+            char ch = (i==s.length()) ? '+' : s[i];
+
+            if(isdigit(ch)){
+                num = num*10 + (ch-'0');
+            }else if(ch != ' '){
+                if(op == '+') st.push(num);
+                else if(op == '-') st.push(-num);
+                else if(op == '*'){
+                    int top = st.top();
+                    st.pop();
+                    st.push(top*num);
+                }
+                else if(op == '/'){
+                    int top = st.top();
+                    st.pop();
+                    st.push(top/num);
+                }
+                op = ch;
+                num = 0;
+            }
+        }
+
+        int ans = 0;
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+
+        return ans;
+    }
+};
