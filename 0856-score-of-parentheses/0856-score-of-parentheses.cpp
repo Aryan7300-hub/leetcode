eca2593f8fff0be7bd0260428ee4786877a00a81
Aryan7300-hub <aryanjindal7300@gmail.com>
2026-08-12 22:31:03 +0530

    Time: 0 ms (100%), Space: 8.1 MB (43.89%) - LeetHub

---
+++ 0856-score-of-parentheses/0856-score-of-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        stack<int>st;
+        st.push(0);
+
+        for(char ch : s){
+            if(ch == '('){
+                st.push(0);
+            }else{
+                int val = st.top();
+                st.pop();
+                int score = max(2*val, 1);
+                int prev = st.top();
+                st.pop();
+                st.push(score+prev);
+            }
+        }
+        return st.top();
+    }
+};
