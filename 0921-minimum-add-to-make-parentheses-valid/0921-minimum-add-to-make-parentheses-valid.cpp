d8e05f769c83538262fc3b06cb3f844598ca1ea4
Aryan7300-hub <aryanjindal7300@gmail.com>
2026-08-12 22:13:05 +0530

    Time: 0 ms (100%), Space: 8.4 MB (82.34%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        stack<int>st;
+        int open = 0, close = 0;
 
         for(char ch : s){
-            if(ch == ')' &&  !st.empty() && st.top() == '('){
-                st.pop();
+            if(ch == '('){
+                open++;
             }else{
-                st.push(ch);
+                if(open>0){
+                    open--;
+                }else{
+                    close++;
+                }
             }
         }
-
-        return st.size();
+        return open + close;
     }
 };
