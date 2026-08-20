8a8fce79b42e7132833f5d05530150b7dd74214a
Aryan7300-hub <aryanjindal7300@gmail.com>
2026-08-20 23:20:40 +0530

    Time: 0 ms (100%), Space: 15.6 MB (82.55%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,18 +11,30 @@
  */
 class Solution {
 public:
-    vector<vector<int>>ans;
-    vector<vector<int>>nodes;
-
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        dfs(root, 0, 0);
+        vector<vector<int>>ans;
+        vector<vector<int>>nodes;
+
+        if(!root) return ans;
+
+        queue<tuple<TreeNode*, int , int>>q;
+        q.push({root, 0, 0});
+
+        while(!q.empty()){
+            auto [node, row, col] = q.front();
+            q.pop();
+            nodes.push_back({col, row, node->val});
+
+            if(node->left) q.push({node->left, row+1, col-1});
+            if(node->right) q.push({node->right, row+1, col+1});
+        }
 
         sort(nodes.begin(), nodes.end());
 
         int prevCol = INT_MAX;
 
         for(auto &node: nodes){
-            if(node[0] != prevCol){
+            if(node[0]!=prevCol){
                 ans.push_back({});
                 prevCol = node[0];
             }
@@ -30,13 +42,5 @@ public:
         }
 
         return ans;
-    }
-private:
-    void dfs(TreeNode* root, int row, int col){
-        if(!root) return;
-        nodes.push_back({col, row, root->val});
-
-        dfs(root->left, row+1, col-1);
-        dfs(root->right, row+1, col+1);
     }
 };
