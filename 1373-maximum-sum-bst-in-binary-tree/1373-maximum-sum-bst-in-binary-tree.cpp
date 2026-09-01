978f5bf3872f37a11171acccf75f96330dd9e76e
Aryan7300-hub <aryanjindal7300@gmail.com>
2026-09-01 23:39:14 +0530

    Time: 730 ms (14.97%), Space: 235.4 MB (6.68%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -18,21 +18,20 @@ public:
     }
 private:
     vector<int> solve(TreeNode* root){
-        if(!root) return{1, INT_MAX, INT_MIN, 0};
+        if(!root) return {1, INT_MAX, INT_MIN, 0};
 
         vector<int>left = solve(root->left);
         vector<int>right = solve(root->right);
 
-        if(left[0] == 1 && right[0] == 1 && left[2] < root->val && root->val < right[1]){
+        if(left[0] == 1 && right[0] == 1 && left[2] < root->val && right[1] > root->val){
             int sum = left[3] + right[3] + root->val;
-            maxSum = max(maxSum, sum);
+            maxSum = max(sum, maxSum);
 
             int minimum = min(left[1], root->val);
             int maximum = max(right[2], root->val);
 
             return {1, minimum, maximum, sum};
         }
-
         return {0, 0, 0, 0};
     }
 };
