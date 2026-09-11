1cc3c93d6143e5d765f571323331a73a6e88acd5
Aryan7300-hub <aryanjindal7300@gmail.com>
2026-09-12 00:35:55 +0530

    Time: 6 ms (65.86%), Space: 19 MB (74.54%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -16,29 +16,28 @@ public:
         if(!root) return 0;
 
         queue<TreeNode*>q;
+
         q.push(root);
-        
+
         while(!q.empty()){
             TreeNode* node = q.front();
             q.pop();
-
-            dfs(node,0, targetSum);
+            dfs(node, targetSum);
             if(node->left) q.push(node->left);
             if(node->right) q.push(node->right);
         }
 
         return count;
-
-        return count;
     }
 private:
-    void dfs(TreeNode* root, long long sum, long long targetSum){
-        if(!root) return;
+    void dfs(TreeNode* root, long long targetSum){
+        if(!root)
+            return;
 
-        sum+=root->val;
-        if(sum == targetSum) count++;
+        if(root->val == targetSum)
+            count++;
 
-        dfs(root->left, sum, targetSum);
-        dfs(root->right, sum, targetSum);
+        dfs(root->left, targetSum-root->val);
+        dfs(root->right, targetSum - root->val);
     }
 };
